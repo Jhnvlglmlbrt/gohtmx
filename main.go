31c77afa9eeeb1c12f184797de256f60a396455d
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2024-02-17 17:57:57 +0300

    changed data structure and info

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"time"
 )
 
-type Film struct {
-	Title    string
-	Director string
+type Product struct {
+	Name     string
+	Producer string
 }
 
 func main() {
@@ -18,25 +18,25 @@ func main() {
 
 	handlerData := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		films := map[string][]Film{
-			"Films": {
-				{Title: "Edge of Tomorrow", Director: "Doug Liman"},
-				{Title: "Breaking Bad", Director: "Vince Gilligan"},
-				{Title: "Troy", Director: "Wolfgang Petersen"},
+		products := map[string][]Product{
+			"Products": {
+				{Name: "iPhone 13", Producer: "Apple"},
+				{Name: "Galaxy S21", Producer: "Samsung"},
+				{Name: "Pixel 6", Producer: "Google"},
 			},
 		}
-		tmpl.Execute(w, films)
+		tmpl.Execute(w, products)
 	}
 
 	handlerList := func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Second)
-		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
+		name := r.PostFormValue("name")
+		producer := r.PostFormValue("producer")
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+		tmpl.ExecuteTemplate(w, "product-list-element", Product{Name: name, Producer: producer})
 	}
 	http.HandleFunc("/", handlerData)
-	http.HandleFunc("/add-film/", handlerList)
+	http.HandleFunc("/add-product/", handlerList)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
